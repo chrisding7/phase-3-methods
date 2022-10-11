ef23308b0e00316c94fc8304e9e573385684dcd5
Chris Ding <chrisding7@berkeley.edu>
2022-10-11 10:38:39 -0700

    commit tree e4797f4c8a6ed95e86ca7e38be34d12003e4d9b6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    greeting = "Hello, programmer!"
+    puts greeting
+    greeting
+end
+
+def greet(name)
+    greeting = "Hello, #{name}!"
+    puts greeting
+    greeting
+end
+    
+
+def greet_with_default(name = "programmer")
+    greeting = "Hello, #{name}!"
+    puts greeting
+    greeting
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number) 
+    if number.class != Integer
+        return nil
+    end
+    
+    number / 2
+end
+
